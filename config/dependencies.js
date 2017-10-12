8116d0be7316d0e08d57f16731345f2624cc0817
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2017-10-12 15:09:41 -0300

    Importação dos scripts e estilos ao index.html

--- config/dependencies.js
+++ config/dependencies.js
@@ -1,19 +1,25 @@
 module.exports = {
+    options: {
+        basePublic: './public/',
+        destCssFolder: 'lib/css/',
+        destJsFolder: 'lib/js/',
+        mainIndex: './public/index.html'
+    },
     css: [
         './node_modules/angular-material/angular-material.min.css',
         './node_modules/angular-material-icons/angular-material-icons.css'
     ],
     js: [
-        './node_modules/angular-animate/angular-animate.min.js',
-        './node_modules/angular-animate/angular-animate.min.js.map',
         './node_modules/angular/angular.min.js',
         './node_modules/angular/angular.min.js.map',
+        './node_modules/angular-route/angular-route.min.js',
+        './node_modules/angular-route/angular-route.min.js.map',
+        './node_modules/angular-animate/angular-animate.min.js',
+        './node_modules/angular-animate/angular-animate.min.js.map',
         './node_modules/angular-aria/angular-aria.min.js',
         './node_modules/angular-aria/angular-aria.min.js.map',
         './node_modules/angular-messages/angular-messages.min.js',
         './node_modules/angular-messages/angular-messages.min.js.map',
-        './node_modules/angular-route/angular-route.min.js',
-        './node_modules/angular-route/angular-route.min.js.map',
         './node_modules/angular-material/angular-material.min.js',
         './node_modules/angular-material-icons/angular-material-icons.min.js'
     ]
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,75 @@
-var gulp         = require('gulp');
-var del          = require('del');
-var dependencies = require('./config/dependencies');
+var gulp          = require('gulp');
+var del           = require('del');
+var inject        = require('gulp-inject-string');
+var stripLine     = require('gulp-strip-line');
+var emptyLine     = require('gulp-remove-empty-lines');
+var dependencies  = require('./config/dependencies');
+var rename        = require('gulp-rename');
 
-gulp.task('copy', function() {
+gulp.task('install', function() {
+    // Copia as bibliotecas para o destino
+    //
     dependencies.css.forEach(function(dependency) {
-        gulp.src(dependency).pipe(gulp.dest('./public/lib/css'));
+        gulp.src(dependency)
+            .pipe(gulp.dest(dependencies.options.basePublic + dependencies.options.destCssFolder));
+    });
+    dependencies.js.forEach(function(dependency) {
+        gulp.src(dependency)
+            .pipe(gulp.dest(dependencies.options.basePublic + dependencies.options.destJsFolder));  
     });
 
-    dependencies.js.forEach(function(dependency) {
-        gulp.src(dependency).pipe(gulp.dest('./public/lib/js'));
-    });
+    // Adiciona as dependências do css ao arquivo principal do projeto
+    //
+    var listCssDependencies = getListCssDependencies();
+    var listJsDependencies  = getListJsDependencies();
+    gulp.src(dependencies.options.mainIndex)
+        .pipe(inject.after('<!--libCss-->', listCssDependencies))
+        .pipe(inject.after('<!--libJs-->', listJsDependencies))
+        .pipe(gulp.dest(dependencies.options.basePublic));
 });
 
-// Limpa os diretórios onde ficam as libs principais, caso queira incrementar o seu gulpfile.
+// Limpa os diretórios onde ficam as libs principais e retira as tags que mencionam as bibliotecas
 //
-gulp.task('clean', function() {
+gulp.task('uninstall', function() {
+    gulp.src(dependencies.options.mainIndex)
+        .pipe(stripLine(['<script', '<link rel="stylesheet"']))
+        .pipe(emptyLine({removeComments: false}))
+        .pipe(gulp.dest(dependencies.options.basePublic));
+
     return del([
         'public/lib/css/**/*',
         'public/lib/js/**/*',
     ]);
 });
+
+/**
+ * Obtém a lista de dependências do css, e gera uma string que será adicionada
+ * ao arquivo principal.
+*/
+function getListCssDependencies() {
+    return dependencies.css.filter(function(dependency){ return dependency.endsWith('.css')})
+    .map(function(dependency) {
+        let dependencyArray = dependency.split('/');
+        let library         = dependencyArray[dependencyArray.length - 1];
+        return `\n\t<link rel="stylesheet" href="${dependencies.options.destCssFolder}${library}"/>`;
+    })
+    .reduce(function(total, linkDependency) {
+        return total + linkDependency
+    }, "");
+}
+
+/**
+ * Obtém a lista de dependências do js, e gera uma string que será adicionada
+ * ao arquivo principal.
+*/
+function getListJsDependencies() {
+    return dependencies.js.filter(function(dependency){ return dependency.endsWith('.js')})
+    .map(function(dependency) {
+        let dependencyArray = dependency.split('/');
+        let library         = dependencyArray[dependencyArray.length - 1];
+        return `\n\t<script src="${dependencies.options.destJsFolder}${library}"></script>`;
+    })
+    .reduce(function(total, linkDependency) {
+        return total + linkDependency
+    }, "");
+}
